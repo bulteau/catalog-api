2907a8c978ac5e80d97e353300388bf9217479e5
François Bulteau <bulteau@MacBook-Pro-de-Francois-2.local>
2019-09-26 23:25:49 +0200

    step1 - add ORM sequelize with postgres connection

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ Expose a product recommandation API
 ## How to use it
 
 ### Step 1 - loading csv data
-**prerequisite :** CSV file with comma delimiter
+**prerequisite :**
+- CSV file with comma delimiter
+- Postgres DB available, you can install it quickly with Docker `docker run --name catalog-postgres -e POSTGRES_PASSWORD=kT6xMdTqahVCvYAN -e POSTGRES_USER=catalog-user -e POSTGRES_DB=catalog-db -d postgres`
 
 Launch this command line to load your data
 `node step1-loading.js path/to/file.csv`
--- step1-loading.js
+++ step1-loading.js
@@ -1,6 +1,52 @@
-const csv = require('csv-parser')
-const fs = require('fs')
+const csv = require('csv-parser');
+const fs = require('fs');
+const { Client } = require("pg");
+const Sequelize = require('sequelize');
 const results = [];
+
+const sequelize = new Sequelize('catalog-db', 'catalog-user', 'kT6xMdTqahVCvYAN', {
+  host: 'localhost',
+  port: 54320,
+  dialect: 'postgres'
+});
+
+const Model = Sequelize.Model;
+class Product extends Model {}
+Product.init({
+  // attributes
+  id: {
+    type: Sequelize.STRING,
+    primaryKey: true,
+    allowNull: false
+  },
+  title: {
+    type: Sequelize.STRING
+  },
+  gender_id: {
+    type: Sequelize.STRING
+  },
+  composition: {
+    type: Sequelize.STRING
+  },
+  sleeve: {
+    type: Sequelize.STRING
+  },
+  photo: {
+    type: Sequelize.STRING
+  },
+  url: {
+    type: Sequelize.STRING
+  },
+}, {
+  sequelize,
+  modelName: 'product'
+});
+
+// Temporary - delete all products
+sequelize.models.product.destroy({
+  where: {},
+  truncate: true
+})
 
 var args = process.argv.slice(2)[0];
 if(args && args.length > 0) {
@@ -8,14 +54,25 @@ if(args && args.length > 0) {
     .pipe(csv({ separator: ';' }))
     .on('data', (data) => results.push(data))
     .on('end', () => {
-      results.forEach(function (value) {
-        console.log(value.id);
-      });
-      // [
-      //   { NAME: 'Daffy Duck', AGE: '24' },
-      //   { NAME: 'Bugs Bunny', AGE: '22' }
-      // ]
+        sequelize.sync()
+          .then(
+            sequelize.models.product.bulkCreate(results, {returning: true})
+            .then(function(response){
+                console.log(response);
+            })
+            .catch(function(error){
+                console.log(error);
+            })
+        ).then(
+
+        );
+
     });
 } else {
   console.log("Wrong parameter");
 }
+
+// Temporary - Find all products
+/*Product.findAll().then(products => {
+  console.log("All products:", JSON.stringify(products, null, 4));
+});*/
