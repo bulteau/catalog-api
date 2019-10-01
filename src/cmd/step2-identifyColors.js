5c828e556f3924fe9d8ab7a0bbe3ff7d3cec0c59
François Bulteau <francois.bulteau@gmail.com>
2019-10-02 00:03:00 +0200

    Step 2 - save image before request google vision api

--- src/cmd/step2-identifyColors.js
+++ src/cmd/step2-identifyColors.js
@@ -1,27 +1,54 @@
 const { Product } = require('../../sequelize');
 const vision = require('@google-cloud/vision');
 const { rgb2lab } = require('colour-proximity');
+const download = require('image-downloader');
 
 // Get dominant color from Google Vision API
 async function getAndUpdateDominantColor(product) {
-  // Creates a client
-  const client = new vision.ImageAnnotatorClient();
+  // Download Image -
+  // This is a bypass for a code error returned by Google Vision API 14 : 'We can not access the URL currently. Please download the content and pass it in.'
+  // more information : https://github.com/googleapis/nodejs-vision/issues/270#issuecomment-481064953
+  const options = {
+    url: 'https:' + product.photo,
+    dest: './data/img/' + product.id + '.jpg'
+  }
+  try {
+    const { filename, image } = await download.image(options);
+    console.log('\x1b[37m', 'Image Saved to', filename, '\x1b[37m');
 
-  // Performs label detection on the image file
-  const [result] = await client.imageProperties('https:' + product.photo);
-  const dominantColors = result.imagePropertiesAnnotation.dominantColors.colors;
-  let colorLab = rgb2lab([
-    dominantColors[0].color.red,
-    dominantColors[0].color.green,
-    dominantColors[0].color.blue]
-  );
-  return await Product.update(
-    { dominant_color_l: parseFloat(colorLab[0]),
-      dominant_color_a: parseFloat(colorLab[1]),
-      dominant_color_b: parseFloat(colorLab[2]),
-      image_properties_annotation: result.image_properties_annotation },
-    { where: { id: product.id } }
-  );
+    // Creates a client
+    const client = new vision.ImageAnnotatorClient();
+
+    // Performs label detection on the image file
+    try {
+      const [result] = await client.imageProperties(filename);
+
+      if(result.imagePropertiesAnnotation) {
+        const dominantColors = result.imagePropertiesAnnotation.dominantColors.colors;
+        let colorLab = rgb2lab([
+          dominantColors[0].color.red,
+          dominantColors[0].color.green,
+          dominantColors[0].color.blue]
+        );
+        return await Product.update(
+          { dominant_color_l: parseFloat(colorLab[0]),
+            dominant_color_a: parseFloat(colorLab[1]),
+            dominant_color_b: parseFloat(colorLab[2]),
+            image_properties_annotation: result.imagePropertiesAnnotation },
+          { where: { id: product.id } }
+        );
+      } else {
+        console.log(product.id);
+        console.log(result);
+        return null;
+      }
+    } catch (e) {
+      console.log(e);
+    }
+
+  } catch (e) {
+    console.log('\x1b[31m', 'Image not found', product.id, '\x1b[37m');
+  }
 }
 
 Product.findAll(
@@ -31,7 +58,7 @@ Product.findAll(
       getAndUpdateDominantColor(product.dataValues)
     ))
     .then(data => {
-      console.log(`Find ${data.length} dominant colors product`);
+      console.log('\x1b[32m', `Founded ${data.filter((e) => e != null).length} / ${data.length} products with a dominant colors `, '\x1b[37m');
     });
 })
 .catch(function(error){
