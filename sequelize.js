7bf65c13a532c23242a0f95f5dac452cb250a0da
François Bulteau <francois.bulteau@gmail.com>
2019-10-02 22:35:23 +0200

    Step 3 - Optimize query for recommandation

--- sequelize.js
+++ sequelize.js
@@ -16,9 +16,9 @@ const Product = ProductModel(sequelize, Sequelize);
 Function using https://github.com/gausie/colour-proximity alogorithm
 maxScore declare a threshold : 0 is maximum proximity, 1 is minimum proximity
 */
-async function requestProximity(l, a, b, maxScore = 10) {
-  return await sequelize.query('SELECT * FROM products WHERE (sqrt(power(dominant_color_l - ?, 2) + power(dominant_color_a - ?, 2) + power(dominant_color_b - ?, 2))) < ?',
-    { replacements: [l, a, b, maxScore], type: sequelize.QueryTypes.SELECT }
+async function requestProximity(l, a, b, limit = 10) {
+  return await sequelize.query('SELECT * FROM products ORDER BY (sqrt(power(dominant_color_l - ?, 2) + power(dominant_color_a - ?, 2) + power(dominant_color_b - ?, 2))) ASC LIMIT ?',
+    { replacements: [l, a, b, limit], type: sequelize.QueryTypes.SELECT }
   );
 }
 
