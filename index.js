8086573a89d67af9ba6997ff1b5fe98d34cddb17
François Bulteau <francois.bulteau@gmail.com>
2019-10-02 22:34:12 +0200

    Add cors with a temporary index config

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+var cors = require('cors');
 const bodyParser = require('body-parser');
 const { Product, requestProximity } = require('./sequelize');
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 // API ENDPOINTS
